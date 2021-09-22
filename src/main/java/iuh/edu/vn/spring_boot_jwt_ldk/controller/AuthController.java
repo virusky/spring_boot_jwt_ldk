35f2602ea2efa98f13849994444f882e4550d2d8
Dĩ Khang <ledikhang2609@gmail.com>
2021-09-22 09:06:11 +0700

    complete master

--- src/main/java/iuh/edu/vn/spring_boot_jwt_ldk/controller/AuthController.java
+++ src/main/java/iuh/edu/vn/spring_boot_jwt_ldk/controller/AuthController.java
@@ -11,5 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AuthController {
 
+    @Autowired
+    private UserService userService;
 
+    @PostMapping("/register")
+    public User register(@RequestBody User user){
+        user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
+
+        return userService.createUser(user);
+    }
 }
