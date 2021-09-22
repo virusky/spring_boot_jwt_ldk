09308367e4d4e2e172f5ceb9f79a0121fa68a579
Dĩ Khang <ledikhang2609@gmail.com>
2021-09-22 09:04:22 +0700

    complete master

--- src/main/java/iuh/edu/vn/spring_boot_jwt_ldk/controller/AuthController.java
+++ src/main/java/iuh/edu/vn/spring_boot_jwt_ldk/controller/AuthController.java
@@ -1,4 +1,15 @@
 package iuh.edu.vn.spring_boot_jwt_ldk.controller;
 
+import iuh.edu.vn.spring_boot_jwt_ldk.entity.User;
+import iuh.edu.vn.spring_boot_jwt_ldk.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AuthController {
+
+
 }
--- src/main/java/iuh/edu/vn/spring_boot_jwt_ldk/service/TokenService.java
+++ src/main/java/iuh/edu/vn/spring_boot_jwt_ldk/service/TokenService.java
@@ -1,6 +1,5 @@
 package iuh.edu.vn.spring_boot_jwt_ldk.service;
 
-import com.example.spring_boot_api_jwt_ad.entity.Token;
 import iuh.edu.vn.spring_boot_jwt_ldk.entity.Token;
 
 public interface TokenService {
